06d10500823460545b590e21a66f83fd06e7345e
HoweBekah <how18019@byui.edu>
2019-12-07 01:56:38 -0700

    first 3 instruction videos done

--- server.js
+++ server.js
@@ -5,6 +5,37 @@ const PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/categories", function(req, res) {
+  //get list of categories
+  console.log("getting categories");
+
+  var results = {
+    categories: [
+      { id: 1, category: "beverages" },
+      { id: 2, category: "side dishes" },
+      { id: 3, category: "main dishes" }
+    ]
+  };
+
+  res.json(results);
+});
+app.get("/category", function(req, res) {
+  //get 1category's recipes by catid
+  console.log("getting category recipes");
+
+  var results = { id: 1, category: "beverages" };
+
+  res.json(results);
+});
+
+app.post("/category", function(req, res) {
+  var recipe = req.body.recipe;
+
+  console.log("creating new recipe: " + recipe);
+  res.json({ success: true });
+});
 
 app.listen(PORT, function() {
   console.log(`Listening on ${PORT}`);
