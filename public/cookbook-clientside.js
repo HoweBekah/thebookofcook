178f9b75b2673b26d50c6dbef296a5e057b60b7f
HoweBekah <how18019@byui.edu>
2019-12-14 17:10:30 -0700

    maybe fixed being able to chage category from recipe

--- public/cookbook-clientside.js
+++ public/cookbook-clientside.js
@@ -61,11 +61,78 @@ function searchByCategory() {
       );
     }
   });
+
+  $.get("/recipe", { category: category }, function(data) {
+    //console.log("Back from server with:");
+    //console.log(data);
+    console.log(category);
+
+    $("#ulRecipes").empty();
+    document.getElementById("allRecipes").style.display = "none";
+    document.getElementById("mainRecTitle").style.display = "none";
+    document.getElementById("ulDiv").style.display = "inherit";
+    document.getElementById("recTitle").style.display = "inherit";
+    document.getElementById("ulRecipes").style.display = "inherit";
+
+    for (var i = 0; i < data.list.length; i++) {
+      var recList = data.list[i];
+
+      $("#ulRecipes").append(
+        `<li onclick="getRecipeById(${recList.recipe_id})">${recList.recipe_name}</li>`
+      );
+    }
+  });
 }
 
 function getRecipeById(recipe_id) {
   //console.log(recipe_id);
   $.get("/recipe", { recipe_id: recipe_id }, function(data) {
+    $("#ulRecipes").empty();
+    $("#recipeDiv").empty();
+    document.getElementById("searchDiv").style.display = "none";
+
+    //$("#recipeDiv").prepend(`<h2>${data.list.recipe_name}</h2>`);
+    for (var i = 0; i < data.list.length; i++) {
+      var recList = data.list[i];
+
+      $("#recipeDiv").append(
+        `<h2 id="recipeTitle">${recList.recipe_name}</h2>`
+      );
+
+      var ingsDiv = document.createElement("div");
+      ingsDiv.id = "ingsDiv";
+      $("#recipeDiv").append(ingsDiv);
+
+      var myList = document.createElement("ul");
+      myList.id = "ingredientList";
+      $("#ingsDiv").append(myList);
+
+      var ingreds = recList.ingredients;
+      var ingList = ingreds.split(",");
+      console.log(ingList);
+      for (var i = 0; i < ingList.length; i++) {
+        if (i % 2 != 0) {
+          console.log(`Odd: ${ingList[i]}`);
+          $("#ingredientList").append(
+            `<li class= "rightIngList">${ingList[i]}</li>`
+          );
+        } else {
+          console.log(`Even: ${ingList[i]}`);
+          $("#ingredientList").append(
+            `<li class= "leftIngList">${ingList[i]}</li>`
+          );
+        }
+      }
+
+      //$("#recipeDiv").append(`<h5>${recList.ingredients}</h5>`);
+      $("#recipeDiv").append(`<p id="instruct">${recList.instructions}</p>`);
+    }
+  });
+}
+
+function getRecipeById(recipe_id) {
+  //console.log(recipe_id);
+  $.put("/recipe", { recipe_id: recipe_id }, function(data) {
     $("#ulRecipes").empty();
     $("#recipeDiv").empty();
     document.getElementById("searchDiv").style.display = "none";
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 //cat gets and posts
 app.get("/categories", catController.getCatList);
 app.get("/search", catController.search);
+app.get("/recipe", catController.search);
 
 //recipes gets and posts
 app.get("/search", recipeController.search);
