88d30ab9bc7b94eef64da21b3fac2583801968d5
HoweBekah <how18019@byui.edu>
2020-02-01 22:53:37 -0700

    new things for oauth maybe

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+var cookieParser = require("cookie-parser");
+var sessions = require("client-sessions");
+var bouncer = require("heroku-bouncer");
 
 const catController = require("./controllers/catController.js");
 const recipeController = require("./controllers/recipeController.js");
@@ -8,6 +11,35 @@ const recipeController = require("./controllers/recipeController.js");
 const PORT = process.env.PORT || 8080;
 
 var app = express();
+
+app.use(cookieParser("your cookie secret"));
+
+app.use(
+  sessions({
+    cookieName: "session",
+    secret: "your session secret",
+    duration: 24 * 60 * 60 * 1000,
+    activeDuration: 1000 * 60 * 5,
+    cookie: {
+      path: "/",
+      ephemeral: false,
+      httpOnly: true,
+      secure: false
+    }
+  })
+);
+
+app.use(
+  bouncer({
+    oAuthClientID: "client-id",
+    oAuthClientSecret: "client-secret",
+    encryptionSecret: "abcd1234abcd1234"
+  })
+);
+
+app.get("/", function(req, res) {
+  res.end("You must be logged in.");
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
